02124d4ef798de11d96e396f2265a104c6d2e4de
Fer Aguirre <kay_op@protonmail.com>
2022-04-17 11:48:33 -0500

    Update project dependencies

--- violencia_obstetrica/data/load.py
+++ violencia_obstetrica/data/load.py
@@ -23,7 +23,6 @@ def load_columns(df):
     return cols
 
 # Inputs
-data_cuba_pre = path.data_processed_dir("data_cuba_preprocessed.xlsx")
 data_cuba = path.data_processed_dir("data_cuba_processed.xlsx")
 data_directorio =  path.data_processed_dir("directorio_hospitales.xlsx")
 data_testimonios =  path.data_processed_dir("testimonios_cuba_processed.xlsx")
